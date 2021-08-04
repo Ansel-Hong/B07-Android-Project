2762e6f23c2415626695db7f6c220d1533c6da34
zhiquan <ericcui127@gmail.com>
2021-08-04 13:11:03 +0800

    1. Updated DoctorActivity.java and activity_doctor.xml.

--- app/src/main/java/com/example/b07project/user_information/DoctorActivity.java
+++ app/src/main/java/com/example/b07project/user_information/DoctorActivity.java
@@ -1,9 +1,87 @@
 package com.example.b07project.user_information;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.b07project.MainActivity;
+import com.example.b07project.R;
+import com.example.b07project.appointment_activities.ViewAppointmentActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DoctorActivity extends AppCompatActivity {
 
+    private FirebaseUser doctor;
+    private DatabaseReference ref;
+    private String userID;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor);
+
+        doctor = FirebaseAuth.getInstance().getCurrentUser();
+        ref = FirebaseDatabase.getInstance().getReference().child("doctors");
+        userID = doctor.getUid();
+
+        final TextView welcome = (TextView) findViewById(R.id.doctor_welcome);
+
+        ref.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Doctor doctorProfile = snapshot.getValue(Doctor.class);
+
+                if (doctorProfile != null){
+                    String name = doctorProfile.name;
+                    String email = doctorProfile.email;
+                    String gender = doctorProfile.gender;
+                    String specialty = doctorProfile.specialty;
+
+                    welcome.setText("Welcome Dr." + name + "!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                new AlertDialog.Builder(DoctorActivity.this)
+                        .setTitle("Something went wrong, please restart the application")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Continue with delete operation.
+                            }
+                        });
+            }
+        });
+
+        Button logout = (Button) findViewById(R.id.doctorSignOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent navigateToDoctorIntent = new Intent(DoctorActivity.this, MainActivity.class);
+                startActivity(navigateToDoctorIntent);
+            }
+        });
+
+        }
+
+        public void navigateToDoctorViewAppointmentsActivity(View view) {
+            Intent navigateToDoctorIntent = new Intent(this, ViewAppointmentActivity.class);
+            startActivity(navigateToDoctorIntent);
+        }
 
 }
