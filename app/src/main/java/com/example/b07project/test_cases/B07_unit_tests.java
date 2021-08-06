3a1e2eb1b1000ad744260ae11175a46685206809
Ansel-Hong <anselhonggibby@gmail.com>
2021-08-06 12:11:53 +0800

    1. Created basic folder and class for JUnit test case and imported testcase in gradle path

---
+++ app/src/main/java/com/example/b07project/test_cases/B07_unit_tests.java
@@ -0,0 +1,10 @@
+package com.example.b07project.test_cases;
+
+import org.junit.Test;
+
+public class B07_unit_tests {
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+
+    }
+}
