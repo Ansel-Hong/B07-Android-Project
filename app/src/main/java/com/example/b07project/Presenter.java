38a5aca447f1958d9c8d8b9883343b1eb4a15702
Ansel-Hong <anselhonggibby@gmail.com>
2021-08-10 01:07:54 +0800

    Removed unused method in Presenter.java

--- app/src/main/java/com/example/b07project/Presenter.java
+++ app/src/main/java/com/example/b07project/Presenter.java
@@ -40,27 +40,4 @@ public class Presenter {
         }
 
     }
-
-    public static String[] checkLoginDetails(String email, String password){
-
-        String[] retVal = new String[2];
-
-        if (email.isEmpty()) {
-            retVal[0] = "Email is required!";
-        }
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            retVal[0] = "Please enter a valid email!";
-        }
-
-        if (password.isEmpty()) {
-            retVal[1] = "Password is required!";
-        }
-
-        if (password.length() < 6) {
-            retVal[1] = "The minimum password length is 6 characters";
-        }
-
-        return retVal;
-    }
 }
