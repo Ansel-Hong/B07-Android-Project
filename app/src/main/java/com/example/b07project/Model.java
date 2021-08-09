346dbae02874dcbd7af237fe157eb4d2de644d7d
Alfred Tze-Hong Ha <alfredhongha@gmail.com>
2021-08-09 09:55:33 -0400

    1. Updated Model 2. Updated userIsFound using the database 3. Not test yet, but will do so soon

--- app/src/main/java/com/example/b07project/Model.java
+++ app/src/main/java/com/example/b07project/Model.java
@@ -1,31 +1,36 @@
 package com.example.b07project;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Model {
-    List<String> emails;
-    List<String> passwords;
+    private FirebaseAuth auth;
 
     public Model(){
-        emails = new ArrayList<String>();
-        passwords = new ArrayList<String>();
-        //add emails and passwords through database?
-        //both doctors and patients?
+        
     }
 
     public boolean userIsFound(String email, String password){
-        //int k = 0;
-       // if(emails.contains(email)){
-            //search in database
-            //if(password matches with the email)
-                return true;
-            //else
-                //return false;
-        //}
 
-        //else
-            //return false;
+        auth = FirebaseAuth.getInstance();
+        boolean [] checker = {false};
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    checker[0] = true;
+            }
+        });
+        return checker[0];
     }
 
 
